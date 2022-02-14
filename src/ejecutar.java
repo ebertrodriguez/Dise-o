d66a2668c172f841a5583c5b5098736b5bdb8f0e
ebertrodriguez <ebertyesitrodriguez@gmail.com>
2022-02-13 23:34:24 -0500

    Se agrega el proyecto

---
+++ src/ejecutar.java
@@ -0,0 +1,11 @@
+
+public class ejecutar {
+	
+public static void main (String args []) {
+	principal texto = new principal();
+	texto.imprimir();
+	texto.contarNumeros();
+	texto.recorrer();
+	texto.llenarArreglo();
+	}
+}
